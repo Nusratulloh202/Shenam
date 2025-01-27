3fa837e12e6a4ff8d7eea270276c2f8c0862e048
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-01-27 17:15:53 +0500

    INFRA:Initialize Unit Test Project

---
+++ Shenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//====================================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//====================================================
+
+namespace Shenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=>    
+            Assert.True(true);
+    }
+}
