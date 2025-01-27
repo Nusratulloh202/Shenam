6dd430cd2a8c1c8b14d5a52ce9d8025a22499c64
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-01-27 16:15:54 +0500

    INFRA:Initialize API Projectt

---
+++ Shenam.API/Startup.cs
@@ -0,0 +1,65 @@
+//====================================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//====================================================
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+namespace Shenam.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)=>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiInfo = new OpenApiInfo
+            {
+                Title = "Shenam.API",
+                Version = "v1"
+            };
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                    name:"v1",
+                    info:apiInfo);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                string url = "/swagger/v1/swagger.json";
+                string name = "Shenam.API v1";
+
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options => 
+                options.SwaggerEndpoint(url, name));
+            }
+                
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+         
+        }
+    }
+}
