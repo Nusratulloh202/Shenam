343783ae23cbe382a8df3932ab8d21c31d0bb359
Nusratulloh Ergashev <102251021+Nusratulloh202@users.noreply.github.com>
2025-01-29 21:12:41 +0500

    CODERUB:Code Error Clean

--- Shenam.API/Startup.cs
+++ Shenam.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Shenam.API
 
             services.AddDbContext<StorageBroker>();
             services.AddControllers();
-            services.AddTransient<IStorageBroker>();
+            services.AddTransient<IStorageBroker, StorageBroker>();
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc(
